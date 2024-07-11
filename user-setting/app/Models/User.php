c5faf9eec13be3dd16ce00f49d0247d4826a384c
mahdi <rahnamamahdi78@gmail.com>
2024-07-11 20:08:30 +0330

    create relation between user and setting

--- user-setting/app/Models/User.php
+++ user-setting/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setting()
+    {
+        return $this->hasOne(Setting::class);
+    }
 }
--- user-setting/database/seeders/DatabaseSeeder.php
+++ user-setting/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Setting;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::create([
+             'name' => 'ali',
+             'email' => 'test@example.com',
+             'password' => '1234',
+         ]);
+         User::create([
+             'name' => 'reza',
+             'email' => 'test2@example.com',
+             'password' => '7896',
+         ]);
+         Setting::create([
+             'theme' => 'light',
+             'font' => 'iran sense',
+             'user_id' => '1',
+         ]);
+         Setting::create([
+             'theme' => 'dark',
+             'font' => 'iran yekan',
+             'user_id' => '2',
+         ]);
     }
 }
