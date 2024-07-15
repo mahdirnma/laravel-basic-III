9bfce99797dce668f100d23c5eb5b9388929ab5b
mahdi <rahnamamahdi78@gmail.com>
2024-07-15 18:28:31 +0330

    start small-shop project

--- small-shop/routes/web.php
+++ small-shop/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $setting=User::with('setting')->get( );
-    return $setting;
+    return view('index');
 });
